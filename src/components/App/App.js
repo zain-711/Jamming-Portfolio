a70dd75d15382666289b881506437a1cc30868e2
Zain Ali <zain3002ali@gmail.com>
2024-10-04 16:38:17 +0100

    Login feature added, obtained access Token

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,54 +5,60 @@ import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap';
 
-const CLIENT_ID="5917513ce3cd477294ff70aa27819777"
-const CLIENT_SECRET="473112741c4b46c09f7759c6a4924599"
 
 function App() {
-
-  
-  // const [searchValue, setSearchValue] = useState("")
-  // const [storedSearch, setStoredSearch] = useState("")
-
   const [searchInput, setSearchInput] = useState("");
-  const [accessToken, setAccessToken] = useState("");
   const [albums, setAlbums] = useState([]);
+  const clientId = '5917513ce3cd477294ff70aa27819777';
+  const redirectUri = 'http://localhost:3000/';
+  const scope = 'user-read-private user-read-email';
+
+
+  const [accessToken, setAccessToken] = useState(null);
 
   useEffect(() => {
-    //API Access Token
-    var authParameters = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+    const params = new URLSearchParams(window.location.hash.substring(1));
+    const token = params.get('access_token');
+    if (token) {
+      setAccessToken(token);
     }
-    fetch('https://accounts.spotify.com/api/token', authParameters)
-      .then(result => result.json())
-      .then(data => setAccessToken(data.access_token))
-  },[])
+  }, []);
+
+  const handleLogin = () => {
+    // Redirect to Spotify authorization endpoint
+    window.location.href = `https://accounts.spotify.com/authorize?${
+      new URLSearchParams({
+        response_type: 'token',
+        client_id: clientId,
+        redirect_uri: redirectUri,
+        scope: scope,
+      }).toString()
+    }`;
+  };
+
 
 
   async function search(){
     console.log("search for " + searchInput) //confirming search is being fetched correctly 
 
-    // Get request Artist ID
-    var searchParameters = {
+
+    const searchParameters = {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer' + accessToken
       }
     }
-    var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', searchParameters)
-      .then(response => response.text())
-      .then(data => {return data.artists.item[0].id})
+
+    const artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', searchParameters)
+      .then(response => response.json())
+      .then(data => { console.log(data) })
 
     console.log("Artist ID is " + artistID)
     // Get request with artist ID gram all albums
 
 
-    var returnedAlbums = await fetch('https://api.spotify.com/v1/artiists/' + artistID + '/albums' + '?inlcude_groups=album&market=UK&limit=30', searchParameters)
+    const returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?inlcude_groups=album&market=US&limit=30', searchParameters)
       .then(response =>  response.text())
       .then(data => {
         console.log(data)
@@ -65,7 +71,12 @@ function App() {
 
   return (
     <div className="App">
+      {!accessToken ? (
+        <button onClick={handleLogin}>Login with Spotify</button>
+      ) : (
+      
       <Container>
+        <h1>Successfully logged in! </h1>
         <InputGroup className='mb-3' size="lg">
           <FormControl placeholder="Search For Artist"
           type='input'
@@ -94,19 +105,8 @@ function App() {
           </Row>
         </Container>
       </Container>
-      {/* <div className='header'>
-        <h1>Jamming</h1>
-        <h3 className='subheading1'>The Spotify playlist creator!</h3>
-        <div className='search'>
-          <label style={{marginRight: 10}}>Search for your favorite song here:</label>
-          <SearchBar value={searchValue} setValue={setSearchValue} storedSearch={storedSearch} setStoredSearch={setStoredSearch}/>        
-        </div>
-      </div>
-      <div>
-        <SearchResults value={searchValue} storedSearch={storedSearch}/>
-      </div> */}
-
-      </div>
+      )} 
+    </div>
   );
 }
 
